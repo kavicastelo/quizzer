de85a385771f0d477298e967f2036d0f074f3642
KAVI <kavindu123450@gmail.com>
2023-04-09 20:32:08 +0530

    6

--- screens/quiz.js
+++ screens/quiz.js
@@ -6,13 +6,17 @@ function Quiz({navigation}) {
     const [question,setQuestion]=useState();
     const [ques,setQues]=useState(0);
     const [option, setOption] = useState([]);
+    const [score, setScore] = useState(0);
+    const [isLoading, setIsLoading] = useState(false);
 
     const getQuiz = async ()=>{
+        setIsLoading(true)
         const url = 'https://opentdb.com/api.php?amount=10&type=multiple&encode=url3986';
         const res = await fetch(url);
         const data = await res.json();
         setQuestion(data.results);
         setOption( generateOptionsAndShuffle(data.results[0]) )
+        setIsLoading(false)
     }
     useEffect(()=>{
         getQuiz()
@@ -36,34 +40,52 @@ function Quiz({navigation}) {
         shuffleArray(options)
         return options
     }
+
+    const handleSelectedOption=(_option)=>{
+        if (_option===question[ques].correct_answer){
+            setScore(score+10);
+        }
+        if (ques !==9){
+            handleOnPressNext();
+        }
+        if (ques === 9){
+            navigation.navigate("Result",{
+                score:score
+            })
+        }
+    }
     return (
         <View style={styles.container}>
-            {question && <View style={styles.parent}>
+            {isLoading ? <View style={{display:'flex', alignItems:'center', justifyContent: 'center', flex: 1}}>
+                <Text>LOADING...</Text>
+            </View> : question && <View style={styles.parent}>
                 <View style={styles.top}>
                 <Text style={styles.que}>Q. {decodeURIComponent( question[ques].question)}</Text>
             </View>
                 <View style={styles.options}>
-                <TouchableOpacity style={styles.optionWrapper}>
+                <TouchableOpacity style={styles.optionWrapper} onPress={()=>handleSelectedOption(option[0])}>
                 <Text style={styles.opt}>{decodeURIComponent( option[0])}</Text>
                 </TouchableOpacity>
-                <TouchableOpacity style={styles.optionWrapper}>
+                <TouchableOpacity style={styles.optionWrapper} onPress={()=>handleSelectedOption(option[1])}>
                 <Text style={styles.opt}>{decodeURIComponent( option[1])}</Text>
                 </TouchableOpacity>
-                <TouchableOpacity style={styles.optionWrapper}>
+                <TouchableOpacity style={styles.optionWrapper} onPress={()=>handleSelectedOption(option[2])}>
                 <Text style={styles.opt}>{decodeURIComponent( option[2])}</Text>
                 </TouchableOpacity>
-                <TouchableOpacity style={styles.optionWrapper}>
+                <TouchableOpacity style={styles.optionWrapper} onPress={()=>handleSelectedOption(option[3])}>
                 <Text style={styles.opt}>{decodeURIComponent( option[3])}</Text>
                 </TouchableOpacity>
                 </View>
                 <View style={styles.bottom}>
-                <TouchableOpacity>
-                <Text>SKIP</Text>
-                </TouchableOpacity>
+                {/*<TouchableOpacity>*/}
+                {/*<Text>SKIP</Text>*/}
+                {/*</TouchableOpacity>*/}
                     {ques !== 9 && <TouchableOpacity onPress={handleOnPressNext}>
-                        <Text>NEXT</Text>
+                        <Text>SKIP</Text>
                     </TouchableOpacity>}
-                    {ques === 9 && <TouchableOpacity onPress={handleOnPressNext}>
+                    {ques === 9 && <TouchableOpacity onPress={()=>{navigation.navigate("Result",{
+                        score:score
+                    })}} >
                         <Text>RESULTS</Text>
                     </TouchableOpacity>}
                 </View>
--- screens/result.js
+++ screens/result.js
@@ -2,23 +2,31 @@ import React from 'react';
 import {Image, StyleSheet, Text, View, TouchableOpacity} from "react-native";
 import {StatusBar} from "expo-status-bar";
 
-function Result(navigation) {
+function Result({navigation, route}) {
+    const {score} = route.params;
+    let resultBanner = '';
+    if (score<40){
+        resultBanner =require('../assets/imgs/not good.png')
+    }
+    else if (score >40 && score <80){
+        resultBanner =require('../assets/imgs/good.png')
+    }
+    else{
+        resultBanner =require('../assets/imgs/awsome.png')
+    }
     return (
-        <View>
-            <View>
-                <Text>result</Text>
-            </View>
+        <View style={styles.container}>
+            <Text style={styles.title}>RESULTS</Text>
+            <Text style={styles.score}>{score}</Text>
             <View style={styles.bannerContainer}>
                 <Image
-                    source={require('../assets/imgs/home.png')}
+                    source={resultBanner}
                     style={styles.banner} resizeMode="contain"/>
             </View>
-            <View>
-                <TouchableOpacity onPress={()=>{navigation.navigate("Home")}}>
-                    <Text>Home</Text>
-                </TouchableOpacity>
-            </View>
-            <StatusBar style="auto" />
+            <TouchableOpacity onPress={()=>{navigation.navigate("Home")}} style={styles.btn}>
+                <Text style={styles.btnTxt}>GO BACK HOME</Text>
+            </TouchableOpacity>
+            <StatusBar style="auto"/>
         </View>
     );
 }
@@ -27,11 +35,41 @@ export default Result;
 
 const styles = StyleSheet.create({
     banner:{
-        width:300,
-        height:300
+        width:'100%',
+        height:'100%'
     },
     bannerContainer:{
+        flex:1,
+        alignItems:"center",
         justifyContent:"center",
-        alignItems:"center"
+        width:'100%'
+    },
+    container:{
+        justifyContent:"center",
+        alignItems:"center",
+        padding:20,
+        height:'100%'
+    },
+    btn:{
+        width:'100%',
+        padding:10,
+        borderRadius:20,
+        alignItems:"center",
+        backgroundColor:'#3498db',
+        marginBottom:20,
+    },
+    btnTxt:{
+        color:'white',
+        fontSize:26,
+        fontWeight:400
+    },
+    title:{
+        fontSize:40,
+        fontWeight:600,
+        marginVertical:50
+    },
+    score:{
+        fontSize:60,
+        fontWeight:800
     }
 })
